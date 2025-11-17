84c14054869c7a6abf88a4610c4405578d200c54
Kauan Nunes Santos Vita <kauanvita@gmail.com>
2025-11-16 23:37:53 -0300

    feat: Adiciona feedback de "Processando" no envio do formulário
    - Adiciona o HTML do loader em `base.html`.
    
    - Adiciona os estilos `.loading-overlay` e `.spinner` em `style.css`.

--- static/script.js
+++ static/script.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const fileInput = document.getElementById("file");
-  const fileDisplay = document.getElementById("file-name-display");
+  const fileLabelSpan = document.querySelector(".custom-file-upload span");
 
-  if (fileInput && fileDisplay) {
+  if (fileInput && fileLabelSpan) {
     fileInput.addEventListener("change", function (e) {
       if (e.target.files.length > 0) {
-        fileDisplay.textContent = e.target.files[0].name;
+        fileLabelSpan.innerText = e.target.files[0].name;
       } else {
-        fileDisplay.textContent = "Nenhum arquivo selecionado";
+        fileLabelSpan.innerText = "Clique para selecionar a planilha";
       }
+    });
+  }
+
+  const uploadForm = document.querySelector(".form-upload");
+  const loader = document.getElementById("loading-overlay");
+
+  if (loader) {
+    loader.style.display = "none";
+  }
+
+  if (uploadForm && loader) {
+    uploadForm.addEventListener("submit", function () {
+      loader.style.display = "flex";
     });
   }
 });
